ae9b3406c2f9d536d592094257bf5716a664d376
Mohamed-Yassine MADDOURI <m.y.maddouri@gmail.com>
2016-04-18 01:49:47 +0200

    Fix ssue #1: if url has #

--- src/io/github/maddouri/intellij/OnlineSearch/state/PluginSettings.java
+++ src/io/github/maddouri/intellij/OnlineSearch/state/PluginSettings.java
@@ -20,37 +20,50 @@ import java.util.ArrayList;
     name = "PluginSettings",
     storages = {
         @com.intellij.openapi.components.Storage(
+            /* @TODO use `value` instead of `file` to get rid of the "deprecated" warning */
             file = StoragePathMacros.APP_CONFIG + "/OnlineSearch_v1_PluginSettings.xml"
         )
     }
 )
 public class PluginSettings implements PersistentStateComponent<PluginSettings> {
 
+    ///
+    public static final String DEFAULT_QUERY_PLACEHOLDER = "#";
+
     public static class SearchEngine implements Serializable {
-        public String name = "";
-        public String url  = "";
+
+        public String name             = "";
+        public String url              = "";
+        public String queryPlaceholder = "";  /* setting this to "" will cause the configuration GUI to set DEFAULT_QUERY_PLACEHOLDER
+                                               * as the placeholder value (cf. {@link SettingsConfigurable.SettingsPanel.SearchEngineEntry#SearchEngineEntry(final PluginSettings.SearchEngine searchEngine)}),
+                                               * which will ultimately cause the addition of the `<option name="queryPlaceholder" />` tag under the pre-1.1.1 `<SearchEngine>` entries in the config file.
+                                               * NB: the config file (and, therefore, the pre-1.1.1 entries) will only be updated if the search engine list has changed (e.g. adding/removing engines and/or editing the existing ones)
+                                               */
 
         public SearchEngine() {
         }
 
-        public SearchEngine(String name, String url) {
-            this.name = name;
-            this.url  = url;
+        public SearchEngine(String name, String url, String queryPlaceholder) {
+            this.name             = name;
+            this.url              = url;
+            this.queryPlaceholder = queryPlaceholder;
         }
     }
 
-    ///
-    public static final String SEARCH_QUERY_PLACEHOLDER = "#";
-
     @com.intellij.util.xmlb.annotations.Transient  // exclude from state
     public static final ArrayList<SearchEngine> DEFAULT_SEARCH_ENGINES = new ArrayList<SearchEngine>(){{  // https://stackoverflow.com/a/1958961/865719
-        add(new SearchEngine("_Google"       , "https://www.google.com/search?q="                    + SEARCH_QUERY_PLACEHOLDER));
-        add(new SearchEngine("Git_hub"       , "https://github.com/search?q="                        + SEARCH_QUERY_PLACEHOLDER));
-        add(new SearchEngine("_SearchCode"   , "https://searchcode.com/?q="                          + SEARCH_QUERY_PLACEHOLDER));
-        add(new SearchEngine("G_repCode"     , "http://grepcode.com/search/?query="                  + SEARCH_QUERY_PLACEHOLDER));  // not https :(
-        add(new SearchEngine("_CppReference" , "http://en.cppreference.com/mwiki/index.php?search="  + SEARCH_QUERY_PLACEHOLDER));  // not https :(
-        add(new SearchEngine("C_Make"        , "https://cmake.org/cmake/help/latest/search.html?q="  + SEARCH_QUERY_PLACEHOLDER));
+        add(new SearchEngine("_Google"       , "https://www.google.com/search?q="                    + DEFAULT_QUERY_PLACEHOLDER, DEFAULT_QUERY_PLACEHOLDER));
+        add(new SearchEngine("Git_hub"       , "https://github.com/search?q="                        + DEFAULT_QUERY_PLACEHOLDER, DEFAULT_QUERY_PLACEHOLDER));
+        add(new SearchEngine("_SearchCode"   , "https://searchcode.com/?q="                          + DEFAULT_QUERY_PLACEHOLDER, DEFAULT_QUERY_PLACEHOLDER));
+        add(new SearchEngine("G_repCode"     , "http://grepcode.com/search/?query="                  + DEFAULT_QUERY_PLACEHOLDER, DEFAULT_QUERY_PLACEHOLDER));  // not https :(
+        add(new SearchEngine("_CppReference" , "http://en.cppreference.com/mwiki/index.php?search="  + DEFAULT_QUERY_PLACEHOLDER, DEFAULT_QUERY_PLACEHOLDER));  // not https :(
+        add(new SearchEngine("C_Make"        , "https://cmake.org/cmake/help/latest/search.html?q="  + DEFAULT_QUERY_PLACEHOLDER, DEFAULT_QUERY_PLACEHOLDER));
     }};
+
+    /** The list of search engines
+     *
+     *  This will be saved in the config file.
+     */
     public ArrayList<SearchEngine> searchEngines = new ArrayList<>();
 
     public PluginSettings() {
